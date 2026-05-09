432efd55a02045d5b8a84dff0f346ece932f2058
m-ade-maulana <ademaulana464@gmail.com>
2026-05-09 10:22:48 +0700

    09/05/2026

--- database/seeders/AccountSeeder.php
+++ database/seeders/AccountSeeder.php
@@ -16,8 +16,9 @@ class AccountSeeder extends Seeder
     public function run(): void
     {
         $accounts = [
-            ['nomor_registrasi' => '792574', 'nama' => 'Ikhsanul Rohim Lubis', 'nisn' => '', 'jurusan' => 'TJKT', 'kategori' => 'rpl_tkj', 'jenis_umum' => 'umum_rpl_tkj', 'jenis_kejuruan' => 'kejuruan_rpl_tkj', 'jenis_kelamin' => 'Perempuan', 'id_gelombang' => '3', 'status' => 'nonaktif'],
-            ['nomor_registrasi' => '827938', 'nama' => 'Harmonie Annisa Dheazelova', 'nisn' => '', 'jurusan' => 'BP', 'kategori' => 'an_dkv_bp', 'jenis_umum' => 'umum_an_dkv_bp', 'jenis_kejuruan' => 'kejuruan_an_dkv_bp', 'jenis_kelamin' => 'Laki-laki', 'id_gelombang' => '3', 'status' => 'nonaktif'],
+            ['nomor_registrasi' => '792574', 'nama' => 'Ikhsanul Rohim Lubis', 'nisn' => '', 'jurusan' => 'TJKT', 'kategori' => 'rpl_tkj', 'jenis_umum' => 'umum_rpl_tkj', 'jenis_kejuruan' => 'kejuruan_rpl_tkj', 'jenis_kelamin' => 'Laki - Laki', 'id_gelombang' => '3', 'status' => 'nonaktif'],
+            ['nomor_registrasi' => '827938', 'nama' => 'Harmonie Annisa Dheazelova', 'nisn' => '', 'jurusan' => 'BP', 'kategori' => 'an_dkv_bp', 'jenis_umum' => 'umum_an_dkv_bp', 'jenis_kejuruan' => 'kejuruan_an_dkv_bp', 'jenis_kelamin' => 'Perempuan', 'id_gelombang' => '3', 'status' => 'nonaktif'],
+            ['nomor_registrasi' => '261894', 'nama' => 'Sultan Haziq Abdillah', 'nisn' => '', 'jurusan' => 'TJKT', 'kategori' => 'rpl_tkj', 'jenis_umum' => 'umum_rpl_tkj', 'jenis_kejuruan' => 'kejuruan_rpl_tkj', 'jenis_kelamin' => 'Laki - Laki', 'id_gelombang' => '3', 'status' => 'nonaktif'],
         ];
         foreach ($accounts as $account) {
             Account::create($account);
