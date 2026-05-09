f8ae2331293b7c16162d41681f8ce2ea9d4d3b5f
m-ade-maulana <ademaulana464@gmail.com>
2026-05-09 12:57:35 +0700

    09/05/2026

--- database/seeders/AccountSeeder.php
+++ database/seeders/AccountSeeder.php
@@ -16,8 +16,7 @@ class AccountSeeder extends Seeder
     public function run(): void
     {
         $accounts = [
-            ['nomor_registrasi' => '827938', 'nama' => 'Harmonie Annisa Dheazelova', 'nisn' => '123456789', 'jurusan' => 'BP', 'kategori' => 'an_dkv_bp', 'jenis_umum' => 'umum_an_dkv_bp', 'jenis_kejuruan' => 'kejuruan_an_dkv_bp', 'jenis_kelamin' => 'Perempuan', 'id_gelombang' => '3', 'status' => 'nonaktif'],
-            ['nomor_registrasi' => '261894', 'nama' => 'Sultan Haziq Abdillah', 'nisn' => '987654321', 'jurusan' => 'TJKT', 'kategori' => 'rpl_tkj', 'jenis_umum' => 'umum_rpl_tkj', 'jenis_kejuruan' => 'kejuruan_rpl_tkj', 'jenis_kelamin' => 'Laki - Laki', 'id_gelombang' => '3', 'status' => 'nonaktif'],
+            ['nomor_registrasi' => '188672', 'nama' => 'Ghina Alifatunnisa', 'nisn' => '1122334455', 'jurusan' => 'DKV', 'kategori' => 'an_dkv_bp', 'jenis_umum' => 'umum_an_dkv_bp', 'jenis_kejuruan' => 'kejuruan_an_dkv_bp', 'jenis_kelamin' => 'Perempuan', 'id_gelombang' => '3', 'status' => 'aktif'],
         ];
         foreach ($accounts as $account) {
             Account::create($account);
