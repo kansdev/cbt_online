10718a27df8a410dd682e80d6db2b7bc962f3d6f
m-ade-maulana <ademaulana464@gmail.com>
2026-05-10 11:34:18 +0700

    Update 10/05/2026

--- database/seeders/AccountSeeder.php
+++ database/seeders/AccountSeeder.php
@@ -16,8 +16,8 @@ class AccountSeeder extends Seeder
     public function run(): void
     {
         $accounts = [
-            ['nomor_registrasi' => '510042', 'nama' => 'Latifah Royyana', 'nisn' => '1111111111', 'jurusan' => 'MP', 'kategori' => 'mp_ak', 'jenis_umum' => 'umum_mp_ak', 'jenis_kejuruan' => 'kejurump_ak', 'jenis_kelamin' => 'Perempuan', 'id_gelombang' => '3', 'status' => 'aktif'],
-            // ['nomor_registrasi' => '950940', 'nama' => 'Jasmine Kyla Rendyta', 'nisn' => '0106818889', 'jurusan' => 'AK', 'kategori' => 'mp_ak', 'jenis_umum' => 'umum_mp_ak', 'jenis_kejuruan' => 'kejuruan_mp_ak', 'jenis_kelamin' => 'Laki - Laki', 'id_gelombang' => '3', 'status' => 'aktif'],
+            ['nomor_registrasi' => '955794', 'nama' => 'Muhammad Rafly Wadson', 'nisn' => '0118059754', 'jurusan' => 'TJKT', 'kategori' => 'rpl_tkj', 'jenis_umum' => 'umum_an_rpl_tkj', 'jenis_kejuruan' => 'kejuruan_rpl_tkj', 'jenis_kelamin' => 'Laki - Laki', 'id_gelombang' => '3', 'status' => 'aktif'],
+            ['nomor_registrasi' => '950940', 'nama' => 'Jasmine Kyla Rendyta', 'nisn' => '0106818889', 'jurusan' => 'AK', 'kategori' => 'mp_ak', 'jenis_umum' => 'umum_mp_ak', 'jenis_kejuruan' => 'kejuruan_mp_ak', 'jenis_kelamin' => 'Laki - Laki', 'id_gelombang' => '3', 'status' => 'aktif'],
         ];
         foreach ($accounts as $account) {
             Account::create($account);
