ef5cb216882132e90027c50549e9200e969b9b38
kansdev <mmaulana69@guru.smk.belajar.id>
2026-06-08 06:22:00 +0700

    08/06/2026

--- resources/views/admin/pages/peserta.blade.php
+++ resources/views/admin/pages/peserta.blade.php
@@ -26,8 +26,8 @@
                             <th>No Registrasi</th>
                             <th>NIS/NISN</th>
                             <th>Nama</th>
-                            <th>Jurusan</th>
-                            <th>Kategori</th>
+                            <th>Jurusan Pertama</th>
+                            <th>Jurusan Kedua</th>
                             <th>Gelombang</th>
                             <th>Status</th>
                         </tr>
@@ -39,8 +39,8 @@
                                 <td>{{ $p->nomor_registrasi }}</td>
                                 <td>{{ $p->nisn }}</td>
                                 <td>{{ $p->nama }}</td>
-                                <td>{{ $p->jurusan }}</td>
-                                <td>{{ $p->kategori }}</td>
+                                <td>{{ $p->jurusan_pertama }}</td>
+                                <td>{{ $p->jurusan_kedua }}</td>
                                 <td>{{ $p->id_gelombang }}</td>
                                 <td>
                                     @if ($p->status == 'nonaktif')
