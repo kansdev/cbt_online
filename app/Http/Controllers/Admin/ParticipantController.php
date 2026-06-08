c8a143f29b0c96aad7e8294105748a0beb24e7c0
kansdev <mmaulana69@guru.smk.belajar.id>
2026-06-08 08:56:55 +0700

    update 08/06/2026

--- app/Http/Controllers/Admin/ParticipantController.php
+++ app/Http/Controllers/Admin/ParticipantController.php
@@ -20,7 +20,7 @@ class ParticipantController extends Controller
     public function peserta()
     {
         // Ambil data peserta dengan pagination 20 data per halaman dan urutkan berdasarkan nama
-        $peserta = Account::orderBy('nama', 'asc')->paginate(20);
+        $peserta = Account::orderBy('nama', 'asc')->paginate(100);
 
         // Tampilkan halaman peserta dengan data peserta
         return view('admin.pages.peserta', compact('peserta'));
@@ -29,7 +29,7 @@ class ParticipantController extends Controller
     public function peserta_aktif()
     {
         // Ambil data peserta yang aktif dengan pagination 20 data per halaman dan urutkan
-        $peserta_aktif = Account::orderBy('nama', 'asc')->paginate(20);
+        $peserta_aktif = Account::orderBy('nama', 'asc')->paginate(100);
         
         // Tampilkan halaman peserta aktif dengan data peserta aktif
         return view('admin.pages.aktif_peserta', compact('peserta_aktif'));
